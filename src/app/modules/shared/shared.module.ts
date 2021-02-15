ec12db36e738be4d2818dad762e9034493bc16d2
Jose Saldaña <jose.saldana@nazaries.com>
2021-02-15 21:21:18 +0100

    Added stars components

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -11,6 +11,9 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { NgCalendarModule } from 'ionic2-calendar';
 import { AppointmentCardComponent } from './components/appointment-card/appointment-card.component';
 import { CalendarModal } from './components/calendar-modal/calendar.modal';
+import { AssessmentCardComponent } from './components/assessment-card/assessment-card.component';
+import { StarsComponent } from './components/stars/stars.component';
+import { StarsInputComponent } from './components/stars-input/stars-input.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { CalendarModal } from './components/calendar-modal/calendar.modal';
     UserFormComponent,
     SearchBarComponent,
     AppointmentCardComponent,
-    CalendarModal
+    CalendarModal,
+    AssessmentCardComponent,
+    StarsComponent,
+    StarsInputComponent
   ],
   imports: [
     CommonModule,
@@ -39,7 +45,10 @@ import { CalendarModal } from './components/calendar-modal/calendar.modal';
     PageLayoutComponent,
     UserFormComponent,
     SearchBarComponent,
-    AppointmentCardComponent
+    AppointmentCardComponent,
+    AssessmentCardComponent,
+    StarsComponent,
+    StarsInputComponent
   ],
   providers: [
     TranslationService
