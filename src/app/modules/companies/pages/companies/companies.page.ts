d00e56f67362e1a97ab42f0f903e9184a50984dd
Jose Saldaña <jose.saldana@nazaries.com>
2021-02-19 15:33:48 +0100

    Added order by to companies

--- src/app/modules/companies/pages/companies/companies.page.ts
+++ src/app/modules/companies/pages/companies/companies.page.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { CompaniesService } from 'src/app/services/companies.service';
 import { Address } from 'src/app/models/address.model';
 import { UtilsService } from 'src/app/services/utils.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-companies',
@@ -14,14 +15,20 @@ export class CompaniesPage implements OnInit {
   companies: Company[] = [];
   filteredCompanies: Company[] = [];
 
+  showFilterModal: boolean = false;
+  filterForm: FormGroup;
+  orderBy = 'default';
+
   constructor(
     private companiesService: CompaniesService,
     private router: Router,
-    private utils: UtilsService
+    private utils: UtilsService,
+    public fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.loadCompanies();
+    this.initFilterForm();
   }
 
   loadCompanies(): void {
@@ -29,12 +36,23 @@ export class CompaniesPage implements OnInit {
       response => {
         this.companies = response.companies;
         this.filteredCompanies = this.companies;
+        this.sortCompanies();
       }
     );
   }
 
+  initFilterForm(): void {
+    this.filterForm = this.fb.group({
+      bussinessType: [''],
+      location: [''],
+      startDate: [''],
+      endDate: ['']
+    });
+  }
+
   searchCompany(search: string): void {
     this.filteredCompanies = search === '' ? this.companies : this.companies.filter(company => this.discardName(company.name, search));
+    this.sortCompanies();
   }
 
   discardName(name, nameSearched) {
@@ -47,5 +65,38 @@ export class CompaniesPage implements OnInit {
 
   printDirection(direction: Address): string {
     return this.utils.printShortDirection(direction);
+  }
+
+  resetFilters(): void {
+    // reset form
+    this.orderBy = 'default';
+    this.sortCompanies();
+  }
+
+  applyFilter(): void {
+    // check form
+    this.sortCompanies();
+  }
+
+  sortCompanies(): void {
+    this.filteredCompanies = this.filteredCompanies.sort((a, b) => {
+      switch (this.orderBy) {
+        case 'valoration':
+          return b.averagePuntuation - a.averagePuntuation;
+        case 'appointment':
+          if (b.fistAvailableAppointment && a.fistAvailableAppointment) {
+            return new Date(a.fistAvailableAppointment.start).getTime() - new Date(b.fistAvailableAppointment.start).getTime();
+          } else if (a.fistAvailableAppointment && !b.fistAvailableAppointment) {
+            return -1;
+          } else if (!a.fistAvailableAppointment && b.fistAvailableAppointment) {
+            return 1;
+          } else {
+            return 0;
+          }
+        case 'distance': // pending
+        default:
+      }
+    })
+    this.showFilterModal = false;
   }
 }
