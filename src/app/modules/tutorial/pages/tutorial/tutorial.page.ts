61862166df3a8faac0df7719f6dfcb0caafaae7a
Jose Saldaña <jose.saldana@nazaries.com>
2021-07-07 08:33:36 +0200

    Added tutorial content

--- src/app/modules/tutorial/pages/tutorial/tutorial.page.ts
+++ src/app/modules/tutorial/pages/tutorial/tutorial.page.ts
@@ -9,13 +9,21 @@ import { UserService } from 'src/app/services/user.service';
 export class TutorialPage implements OnInit {
   userRole: string;
   userContent = [
-    {top: 'TUTORIAL.user.welcome', image: '../../../../../assets/img/user-tutorial/1.png', bot: 'TUTORIAL.user.1'},
-    // {top: 'TUTORIAL.user.welcome', image: '../../../../../assets/img/user-tutorial/2.png', bot: 'TUTORIAL.user.1'},
-    // {top: 'TUTORIAL.user.welcome', image: '../../../../../assets/img/user-tutorial/3.png', bot: 'TUTORIAL.user.1'},
-    // {top: 'TUTORIAL.user.welcome', image: '../../../../../assets/img/user-tutorial/4.png', bot: 'TUTORIAL.user.1'}
+    {top: 'TUTORIAL.welcome', image: '../../../../../assets/img/user-tutorial/1.png', bot: 'TUTORIAL.1'},
+    {top: 'TUTORIAL.user.2-top', image: '../../../../../assets/img/user-tutorial/2.png', bot: 'TUTORIAL.user.2-bot'},
+    {top: 'TUTORIAL.user.3-top', image: '../../../../../assets/img/user-tutorial/3.png', bot: 'TUTORIAL.user.3-bot'},
+    {top: 'TUTORIAL.user.4-top', image: '../../../../../assets/img/user-tutorial/4.png', bot: 'TUTORIAL.user.4-bot'},
+    {top: 'TUTORIAL.user.5-top', image: '../../../../../assets/img/user-tutorial/5.png', bot: 'TUTORIAL.user.5-bot'},
+    {top: 'TUTORIAL.user.6-top', image: '../../../../../assets/img/user-tutorial/6.png', bot: 'TUTORIAL.user.6-bot'}
   ];
   managerContent = [
-    // {top: 'TUTORIAL.user.welcome', image: '../../../../../assets/img/manager-tutorial/0.png', bot: 'TUTORIAL.user.1'}
+    {top: 'TUTORIAL.welcome', image: '../../../../../assets/img/manager-tutorial/1.png', bot: 'TUTORIAL.1'},
+    {top: 'TUTORIAL.manager.2-top', image: '../../../../../assets/img/manager-tutorial/2.png', bot: 'TUTORIAL.manager.2-bot'},
+    {top: 'TUTORIAL.manager.3-top', image: '../../../../../assets/img/manager-tutorial/3.png', bot: 'TUTORIAL.manager.3-bot'},
+    {top: 'TUTORIAL.manager.4-top', image: '../../../../../assets/img/manager-tutorial/4.png', bot: 'TUTORIAL.manager.4-bot'},
+    {top: 'TUTORIAL.manager.5-top', image: '../../../../../assets/img/manager-tutorial/5.png', bot: 'TUTORIAL.manager.5-bot'},
+    {top: 'TUTORIAL.manager.6-top', image: '../../../../../assets/img/manager-tutorial/6.png', bot: 'TUTORIAL.manager.6-bot'},
+    {top: 'TUTORIAL.manager.7-top', image: '../../../../../assets/img/manager-tutorial/7.png', bot: 'TUTORIAL.manager.7-bot'}
   ];
 
   content: any[];
